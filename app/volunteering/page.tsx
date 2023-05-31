49a07561d7b973642bfa98e17fc0cca8c0eef4e5
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-05-31 22:27:46 +0500

    volunteering router

--- app/volunteering/page.tsx
+++ app/volunteering/page.tsx
@@ -1,7 +1,7 @@
-import Container from '@/components/Container';
-import ItemCard from '@/components/ItemCard';
-import { plantRedbook } from '@/data/redbooks';
-import { FunctionComponent } from 'react';
+import Container from "@/components/Container";
+import ItemCard from "@/components/ItemCard";
+import { plantRedbook } from "@/data/redbooks";
+import { FunctionComponent } from "react";
 
 interface VolunteeringProps {}
 
@@ -9,13 +9,24 @@ const Volunteering: FunctionComponent<VolunteeringProps> = () => {
   return (
     <main>
       <Container className="flex flex-col gap-[48px]">
-        <h2 className="volunteering-title font-bold text-BIT_TITLE text-BLACK">Volunteering</h2>
+        <h2 className="volunteering-title font-bold text-BIT_TITLE text-BLACK">
+          Volunteering
+        </h2>
         <section className="red-book-item-list grid grid-cols-3 gap-x-[24px] gap-y-[40px] content-center">
           {plantRedbook
             ? plantRedbook.families[0].items.map((item) => (
-                <ItemCard name={item.name} link={item.img} key={item.id} org="Repetek" />
+                <ItemCard
+                  name={item.name}
+                  img={item.img}
+                  key={item.id}
+                  org="Repetek"
+                  familyId={item.familyId}
+                  id={item.id}
+                  text={item.text}
+                  type="volunteering"
+                />
               ))
-            : 'no items'}
+            : "no items"}
         </section>
       </Container>
     </main>
