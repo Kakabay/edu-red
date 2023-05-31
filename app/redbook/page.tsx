6fc2f37049d2f68b73cf07f0054403646a3e1953
Kakabay <2kakabayashyrberdyew@gmail.com>
2023-05-31 11:08:29 +0500

    redbook page

--- app/redbook/page.tsx
+++ app/redbook/page.tsx
@@ -1,5 +1,20 @@
+import Container from "@/components/Container";
+import MainBlock from "@/components/MainBlock";
+
 const RedBook = () => {
-  return <div></div>;
+  return(
+    <main>
+      <Container className="flex flex-col gap-[48px]">
+        <h2 className="red-book-title font-bold text-BIT_TITLE text-BLACK">
+          Red book
+        </h2>
+          <MainBlock link="/redbook/families" imageLink="/red-book-1.jpg" text="The Red Data Book of Turkmenistan :
+          Plants and Fungi"/>
+          <MainBlock link="/redbook/families" imageLink="/red-book-2.jpg" text="The Red Data Book of Turkmenistan :
+          Invertebrate and Vertebrate Animals"/>
+      </Container>
+    </main>
+  );
 };
 
 export default RedBook;
