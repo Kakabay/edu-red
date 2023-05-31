fd26be2ec86867b7a18cbfd311d1f3e103e4c106
Kakabay <kashyrberdyew@gmail.com>
2023-05-31 19:10:34 +0500

    volunteering

---
+++ app/volunteering/page.tsx
@@ -0,0 +1,25 @@
+import Container from '@/components/Container';
+import ItemCard from '@/components/ItemCard';
+import { plantRedbook } from '@/data/redbooks';
+import { FunctionComponent } from 'react';
+
+interface VolunteeringProps {}
+
+const Volunteering: FunctionComponent<VolunteeringProps> = () => {
+  return (
+    <main>
+      <Container className="flex flex-col gap-[48px]">
+        <h2 className="volunteering-title font-bold text-BIT_TITLE text-BLACK">Volunteering</h2>
+        <section className="red-book-item-list grid grid-cols-3 gap-x-[24px] gap-y-[40px] content-center">
+          {plantRedbook
+            ? plantRedbook.families[0].items.map((item) => (
+                <ItemCard name={item.name} link={item.img} key={item.id} org="Repetek" />
+              ))
+            : 'no items'}
+        </section>
+      </Container>
+    </main>
+  );
+};
+
+export default Volunteering;
