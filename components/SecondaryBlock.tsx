5846f22d52a625d2b84213f17b1ad90ce077a97c
Kakabay <kashyrberdyew@gmail.com>
2023-05-31 19:11:28 +0500

    secondary block

--- components/SecondaryBlock.tsx
+++ components/SecondaryBlock.tsx
@@ -1,21 +1,33 @@
-import Link from "next/link"
-import Image from "next/image"
+import Link from 'next/link';
+import Image from 'next/image';
 
 interface IProps {
-    imageLink: string,
-    text: string,
-    link: string
+  imageLink: string;
+  text: string;
+  link: string;
 }
 
 const SecondaryBlock = ({ imageLink, text, link }: IProps) => {
-    return(
-        <Link href={`${link}`} className="news-main-news block h-[480px] w-[1320px] relative rounded-[16px] overflow-hidden">
-            <Image alt="main news" src={`${imageLink}`} unoptimized unselectable="off" width={644} height={480} className="w-[644px] objet-cover h-full"/>
-            <div className="absolute bottom-0 left-0 w-[644px] h-full flex items-end py-[30px] px-[40px] bg-IMAGE z-10 rounded-[16px]">
-                <h3 className="news-secondary-title text-WHITE font-bold text-SMALL_TITLE max-w-[605px]">{text}</h3>
-            </div>
-        </Link>
-    )
-}
+  return (
+    <Link
+      href={`${link}`}
+      className="news-main-news block h-[480px] w-full relative rounded-[16px] overflow-hidden">
+      <Image
+        alt="main news"
+        src={`${imageLink}`}
+        unoptimized
+        unselectable="off"
+        width={644}
+        height={480}
+        className="w-full objet-cover h-full"
+      />
+      <div className="absolute bottom-0 left-0 w-[644px] h-full flex items-end py-[30px] px-[40px] bg-IMAGE z-10 rounded-[16px]">
+        <h3 className="news-secondary-title text-WHITE font-bold text-SMALL_TITLE max-w-[605px]">
+          {text}
+        </h3>
+      </div>
+    </Link>
+  );
+};
 
 export default SecondaryBlock;
