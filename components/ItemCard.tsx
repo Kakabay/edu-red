453e0b8add0da63952c97e7d666499b5d42bcc97
Kakabay <kashyrberdyew@gmail.com>
2023-06-02 19:27:54 +0500

    images added

--- components/ItemCard.tsx
+++ components/ItemCard.tsx
@@ -1,23 +1,22 @@
-import Link from "next/link";
-import Image from "next/image";
-import { IRedbookItem } from "@/typings/redbook.data.types";
+import Link from 'next/link';
+import Image from 'next/image';
+import { IRedbookItem } from '@/typings/redbook.data.types';
 
 interface IItemCard extends IRedbookItem {
   org?: string;
-  type: "reserves" | "redbook/plants" | "redbook/animals" | "volunteering";
+  type: 'reserves' | 'redbook/plants' | 'redbook/animals' | 'volunteering';
 }
 
 const ItemCard = ({ name, id, img, org, familyId, type }: IItemCard) => {
   return (
     <Link
       href={
-        type === "redbook/plants" || type === "redbook/animals"
+        type === 'redbook/plants' || type === 'redbook/animals'
           ? `/${type}/families/${familyId}/${id}`
           : `/${type}/${id}`
       }
-      className="flex flex-col gap-[16px]"
-    >
-      <div className="item-card-image max-w-[424px] max-h-[240px] overflow-hidden rounded-[16px]">
+      className="flex flex-col gap-[16px]">
+      <div className="item-card-image max-w-[424px] h-[240px] overflow-hidden rounded-[16px]">
         <Image
           alt={`${name}`}
           width={424}
@@ -28,9 +27,7 @@ const ItemCard = ({ name, id, img, org, familyId, type }: IItemCard) => {
       </div>
       <div className="item-card-info flex flex-col gap-[8px] text-BLACK">
         <h2 className="family-item-title text-SMALL_TITLE font-bold">{name}</h2>
-        {org ? (
-          <h2 className="family-item-subtitle text-BIG_TEXT">{org}</h2>
-        ) : null}
+        {org ? <h2 className="family-item-subtitle text-BIG_TEXT">{org}</h2> : null}
       </div>
     </Link>
   );
